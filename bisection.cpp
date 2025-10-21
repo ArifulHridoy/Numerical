be4a45d4ba9688da152ae70a4522bfaae77d3dcf
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-10-22 01:56:57 +0600

    Optimized bisection.

--- bisection.cpp
+++ bisection.cpp
@@ -1,48 +1,78 @@
 #include<iostream>
 #include<cmath>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
 double f(double x)
 {
-    return x*x*x -x-1;
+     return x*x*x*x -5*x*x + 4;
+}
+
+double limCalc(double an, double an1, double an2)
+{
+    return sqrt((an1/an)*(an1/an) -2*(an2/an));
+}
+
+void removeDup(vector<double> &roots, double E)
+{
+    sort(roots.begin(),roots.end());
+    roots.erase(unique(roots.begin(),roots.end(),[&](double a,double b){return fabs(a-b)<E;}),roots.end());
 }
 
 int main()
 {
+    int cnt=0;
     double E,x0,root;
-    vector<double> roots;
+    vector<double>roots;
     cin>>E;
-    for(double i=-10; i<=10;i++)
+    double lim=limCalc(1,0,-5);
+    cout<<"Lim = "<<lim<<endl;
+
+    for(double i=-lim;i<=3; i=i+0.5)
     {
         double x1=i;
-        double x2=i+1;
+        double x2=i+0.5;
 
-        if(f(x1)*f(x2)<0)
+        if(fabs(f(x1))<E)
+        {
+            roots.push_back(x1);
+            cout<<"Root found exactly at x = "<<x1<<endl;
+            continue;
+        }
+
+        if(fabs(f(x2))<E)
+        {
+            roots.push_back(x2);
+            cout<<"Root found exactly at xx = "<<x2<<endl;
+            i+=0.5;
+            continue;
+        }
+
+        else if(f(x1)*f(x2)<0)
         {
             while(fabs(x2-x1)>E)
             {
-            x0=(x1+x2)/2.0; //mid
+                cnt++;
+                x0=(x1+x2)/2.0;
 
-            if(fabs(f(x0))<E)
-            {
-                break;
+                if(fabs(f(x0))<E) break;
+
+                else if(f(x0)*f(x1)<0) x2=x0;
+
+                else x1 = x0;
             }
-
-            else if(f(x0)*f(x1)<0) x2=x0;
-
-            else if(f(x0)*f(x2)<0) x1=x0;
-        }
-
-        root=(x1+x2)/2.0;
-        roots.push_back(root);
-
+            root=(x1+x2)/2.0;
+            roots.push_back(root);
+            cout<<cnt<<" iteration needed"<<endl;
+            cnt=0;
+            cout<<"Root found in ["<<i<<","<<i+0.5<<"] = "<<root<<endl;
         }
     }
-
-    for(int i=0; i<roots.size(); i++)
-    {
-        cout<<roots[i]<<endl;
-    }
-
+    removeDup(roots,E);
+    
+    cout<<"\nAll real roots:\n";
+    for(double r : roots)
+        cout<<r<<endl;
 
 }
